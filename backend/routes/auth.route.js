bb7806b48e23e6121d356bcb92ad5080e9be79c0
Dhvanibarot5 <dhvanibarot5@gmail.com>
2024-11-20 02:35:00 +0530

    starting frontend

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -4,9 +4,8 @@ import { login, logout, signup } from "../controllers/auth.controller.js";
 const router = express.Router();
 
 router.get("/signup", signup);
-
 router.get("/login", login);
-
 router.get("/logout", logout);
 
 export default router;
+//mongodb+srv://dhvanibarot5:123@cluster0.ddk0z.mongodb.net/
